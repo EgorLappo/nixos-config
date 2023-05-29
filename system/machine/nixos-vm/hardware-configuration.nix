22695f65f545bf0505b433d87f7a079673476e7e
Egor Lappo <elappo@stanford.edu>
2023-05-29 03:36:23 -0700

    uuid

--- system/machine/nixos-vm/hardware-configuration.nix
+++ system/machine/nixos-vm/hardware-configuration.nix
@@ -3,13 +3,13 @@
 {
   fileSystems."/boot/efi" =
     {
-      device = "/dev/disk/by-uuid/88B0-4332";
+      device = "/dev/disk/by-uuid/C1F9-0E15";
       fsType = "vfat";
     };
 
   fileSystems."/" =
     {
-      device = "/dev/disk/by-uuid/7c68ba08-6f10-4aba-8ced-7fa34105d756";
+      device = "/dev/disk/by-uuid/a2135e99-fd56-47e4-8409-91a4bed4a381";
       fsType = "ext4";
     };
 }
