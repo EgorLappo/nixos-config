9b8bfcfdf0e3c4b160aee2b83d82fc8b3a9e7d59
Egor Lappo <elappo@stanford.edu>
2023-05-29 21:43:59 -0700

    ..

--- system/wm/xmonad.nix
+++ system/wm/xmonad.nix
@@ -28,7 +28,7 @@
       };
 
       displayManager = {
-        lightdm.background = ./imgs/lock.jpg;
+        lightdm.background = ../../imgs/lock.jpg;
 
         lightdm.greeters.mini = {
           enable = true;
