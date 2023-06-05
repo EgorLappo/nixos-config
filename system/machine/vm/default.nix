ddbfc0adfff42c2217a97bad50aafc1ee574c109
Egor Lappo <elappo@stanford.edu>
2023-06-05 11:30:22 -0700

    generation limit

--- system/machine/vm/default.nix
+++ system/machine/vm/default.nix
@@ -12,6 +12,7 @@
     kernelPackages = pkgs.linuxPackages_latest;
     loader = {
       systemd-boot.enable = true;
+      systemd-boot.configurationLimit = 5;
       efi.canTouchEfiVariables = true;
       efi.efiSysMountPoint = "/boot/efi";
     };
